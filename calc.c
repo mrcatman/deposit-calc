5b19019ab6406493ea7cfdee150c59169692e3bd
Алексеев Кирилл Дмитриевич <ri67s01@mwp09.csc.local>
2017-02-15 08:31:41 +0700

    Make input message more clear

--- calc.c
+++ calc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
 	int sum,time;	
-	printf("Введите сумму вклада и его срок");
+	printf("Введите сумму вклада (в рублях)  и его срок (в днях)\n");
 	scanf("%d %d",&sum,&time);
 }
