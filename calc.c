9c3f393228af07f3cafbd7803feee1796daa712c
Алексеев Кирилл Дмитриевич <ri67s01@mwp09.csc.local>
2017-02-15 08:59:42 +0700

    Add percent calculation

--- calc.c
+++ calc.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 int main() {
-	int sum,time;	
+	int sum,time,percent;	
 	printf("Введите сумму вклада (в рублях) и его срок (в днях)\n");
 	printf("Максимальный срок вклада - 365 дней\n");
 	scanf("%d %d",&sum,&time);
 	if (sum>10000 && time>0 && time<365) {
-		printf("Данные введены корректно\n");
+		if (sum<100000) {
+			if (time<31) {
+				percent=-10;
+			} else {
+				if (time<121) {
+					percent=2;
+				} else {
+					if (time<241) {
+						percent=6;
+					} else {
+						percent=12;
+					}
+				}
+			}
+		} else {
+		  if (time<31) {
+                percent=-10;
+            } else {
+                if (time<121) {
+                    percent=3;
+                } else {
+                    if (time<241) {
+                        percent=8;
+                    } else {
+                        percent=15;
+                    }
+                }
+            }
+		}
+		printf("Процент по вкладу: %d \n",percent);
 	} else {
 		printf("Данные введены некорректно\n");
 	}
